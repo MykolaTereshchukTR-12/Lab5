b858888a74a326096985151c4855ab90a1843a95
Mykola <nikter387@gmail.com>
2024-04-01 10:52:09 +0300

    Commit 2. Creating a method to calculate the area of a square by the length of the side.

--- src/main/java/org/example/Area.java
+++ src/main/java/org/example/Area.java
@@ -2,4 +2,27 @@ package org.example;
 
 public class Area {
 
+    //Square
+    public double areaSquare(double a) {
+        return a*a;
+    }
+
+    //Rectangle
+
+
+    //Parallelogram
+
+
+    //Diamond
+
+
+    //A right triangle
+
+
+    //Trapeze
+
+
+    //Circle
+
+
 }

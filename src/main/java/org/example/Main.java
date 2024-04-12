b574510424841163fa9754651a3b6d7f509026e8
Mykola <nikter387@gmail.com>
2024-04-12 18:59:20 +0300

    Commit 11. Writing an application for user interaction with the program. Unsuccessfully. Fixed a bug in the three() method.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,22 +1,107 @@
 package org.example;
 
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class Main {
+
+    private static int control = 11;
+
+
     public static void main(String[] args) {
         Area area = new Area();
-        System.out.println(area.areaSquare(5));
-        System.out.println(area.areaRectangle(5,4));
-        System.out.println(area.areaParallelogram(5,3));
-        System.out.println(area.areaRhombus(5,(Math.PI)/3));
-        System.out.println(area.areaRightTriangle(3,4));
-        System.out.println(area.areaTrapeze(10,5,5));
-        System.out.println(area.areaCircle(4));
-
         OtherMethods otherMethods = new OtherMethods();
-        System.out.println(otherMethods.three( 3, 4, 5));
-        System.out.println(Arrays.toString(otherMethods.mass(10)));
-        double[] mass = {1,2,3,4,5,6,7,7,8,9,9,0,12,-12344,111,-1};
-        System.out.println(otherMethods.largeNumber(mass, mass.length));
+        Scanner scanner = new Scanner(System.in);
+        Main main = new Main();
+
+        System.out.println("""
+                \nHello! This is a program that performs the tasks of laboratory work in the discipline "Object Oriented Programming". Let's start work!!!
+                For help, enter 11;
+                Enter 0 to exit the program.""");
+
+        while(true){
+            switch(control) {
+                case (0): {
+                    System.out.println("Before meeting!");
+                    System.exit(0);
+                }
+                case (1): {
+                    System.out.println("Enter the actual length of the side of the square:");
+                    System.out.println(area.areaSquare(scanner.nextDouble()));
+                    main.controlSYS();
+                    break;
+                }
+                case (2): {
+                    System.out.println("Enter the actual lengths of the smaller and larger sides of the rectangle:");
+                    System.out.println(area.areaRectangle(scanner.nextDouble(), scanner.nextDouble()));
+                    main.controlSYS();
+                    break;
+                }
+                case (3): {
+                    System.out.println("Enter the actual lengths of the side and the height of the parallelogram:");
+                    System.out.println(area.areaParallelogram(scanner.nextDouble(), scanner.nextDouble()));
+                    main.controlSYS();
+                    break;
+                }
+                case (4): {
+                    System.out.println("Enter the actual length of the rhombus and its smaller angle in radians:");
+                    System.out.println(area.areaRhombus(scanner.nextDouble(), scanner.nextDouble()));
+                    main.controlSYS();
+                    break;
+                }
+                case (5): {
+                    System.out.println("Enter the actual lengths of the legs of a right triangle:");
+                    System.out.println(area.areaRightTriangle(scanner.nextDouble(), scanner.nextDouble()));
+                    main.controlSYS();
+                    break;
+                }
+                case (6): {
+                    System.out.println("Enter the actual lengths of the bases and heights of the trapezium:");
+                    System.out.println(area.areaTrapeze(scanner.nextDouble(), scanner.nextDouble(), scanner.nextDouble()));
+                    main.controlSYS();
+                    break;
+                }
+                case (7): {
+                    System.out.println("Enter the actual length of the circle radius:");
+                    System.out.println(area.areaCircle(scanner.nextDouble()));
+                    main.controlSYS();
+                    break;
+                }
+                case (8): {
+                    System.out.println("Enter three real numbers to find the smallest of them:");
+                    System.out.println(otherMethods.three(scanner.nextDouble(), scanner.nextDouble(), scanner.nextDouble()));
+                    main.controlSYS();
+                    break;
+                }
+                case (9): {
+                    System.out.println("Enter the number of elements of the array that will be generated randomly:");
+                    System.out.println(Arrays.toString(otherMethods.mass(scanner.nextInt())));
+                    main.controlSYS();
+                    break;
+                }
+                case (10): {
+                    System.out.println("Enter first the number of elements that will be in the array, and then enter the elements themselves through a space to find the largest element of the array:");
+                    double[] mass = new double[scanner.nextInt()];
+                    for (int i = 0; i != mass.length; i++) {
+                        mass[i] = scanner.nextDouble();
+                    }
+                    System.out.println(otherMethods.largeNumber(mass, mass.length));
+                    main.controlSYS();
+                    break;
+                }
+                case (11): {
+                    System.out.println("The essence of the program:");
+                    System.out.println(otherMethods.help());
+                    control = scanner.nextInt();
+                    break;
+                }
+            }
+        }
+    }
+
+    public void controlSYS(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Do you want to continue? Enter 11 if yes or 0 to exit the program.");
+        control = scanner.nextInt();
     }
 }
--- src/main/java/org/example/OtherMethods.java
+++ src/main/java/org/example/OtherMethods.java
@@ -2,8 +2,23 @@ package org.example;
 
 public class OtherMethods {
 
+    public String help(){
+        return """
+                To find the area of a square, enter 1;\s
+                To find the area of a rectangle, enter 2;\s
+                To find the area of a parallelogram, enter 3;\s
+                To find the area of a rhombus, enter 4;\s
+                To find the area of a right triangle, enter 5;\s
+                To find the area of a trapezoid, enter 6;\s
+                To find the area of a circle, enter 7;\s
+                To find the smallest number of the three given, enter 8;\s
+                To output an array of random numbers, enter 9;\s
+                To output the largest value from the user-entered array, enter 10;\s
+                Enter 0 to exit the program.""";
+    }
+
     public String three(double a, double b, double c){
-        return "" + Math.max(a, Math.max(b, c));
+        return "" + Math.min(a, Math.min(b, c));
     }
 
     public double[] mass(int len){
